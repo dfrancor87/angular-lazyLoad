070b089ac63771a413b214de569e29b854df9dbb
dfrancor87 <danfer.fra@gmail.com>
2021-09-28 18:27:49 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule)
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('./productos/productos.module').then( module => module.ProductosModule)
+  },
+  {
+    path: 'usuarios',
+    loadChildren: () => import('./usuarios/usuarios.module').then( module => module.UsuariosModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
---
+++ src/app/usuarios/usuarios-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListadoComponent } from '../productos/pages/listado/listado.component';
+import { EditarComponent } from './pages/editar/editar.component';
+import { NuevoComponent } from './pages/nuevo/nuevo.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'nuevo',
+        component: NuevoComponent
+      },
+      {
+        path: 'editar',
+        component: EditarComponent
+      },
+      {
+        path: 'listado',
+        component: ListadoComponent
+      },
+      {
+        path: 'usuario',
+        component: UsuarioComponent
+      },
+      {
+        path: '**',
+        component: ListadoComponent
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsuariosRoutingModule { }
